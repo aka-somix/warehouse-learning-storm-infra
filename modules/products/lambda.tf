a7d86b00e9292303bbf67bbb891c9b86b196aa13
Salvatore Cirone <s.cirone@reply.it>
2022-07-26 15:52:34 +0200

    fix(products): add basic policy to lambda infra

--- modules/products/lambda.tf
+++ modules/products/lambda.tf
@@ -38,6 +38,12 @@ resource "aws_iam_role" "products_handler" {
 EOF
 }
 
+# Lambda Basics Policy (Cloudwatch logs)
+resource "aws_iam_role_policy_attachment" "basic_execution" {
+  role       = aws_iam_role.products_handler.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
 # DynamoDB Products Table Policy
 resource "aws_iam_role_policy_attachment" "dynamodb_products_access" {
   role       = aws_iam_role.products_handler.name
